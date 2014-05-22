e80c5f02907542847425bab337cc25baca8290af
Jan Fredrik Wedén <jfweden@gmail.com>
2014-05-22 10:05:27 +0200

    Latest from SVV

---
+++ install.bat
@@ -0,0 +1,8 @@
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+
+echo
+echo #
+echo # DONE! homestash windows install.
+echo #
+echo
+pause
